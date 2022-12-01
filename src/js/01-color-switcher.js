36539f2cc5395e15ed140dffa1cdb169d70e003b
NikitaSimakov <mishaaniskin@gmail.com>
2022-12-01 20:52:57 +0300

    task 1 & 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+const INTERVAL_DELAY = 1000;
+btnStart.disabled = false;
+btnStop.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function updateBackgroundColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+btnStart.addEventListener('click', onBtnStartClick);
+
+function onBtnStartClick() {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  timerId = setInterval(updateBackgroundColor, INTERVAL_DELAY);
+
+  console.log('Interval was started!');
+}
+
+btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStopClick() {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+
+  clearInterval(timerId);
+  console.log('Interval has stopped...');
+}
